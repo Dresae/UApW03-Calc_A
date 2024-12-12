76e35167b405306d812fca3251fd2f1af11c39b7
Dresae <94129538+Dresae@users.noreply.github.com>
2024-12-11 21:13:16 -0800

    math and error function

--- z.js
+++ z.js
@@ -12,3 +12,24 @@ calculatorIcon.addEventListener('click', () => {
         calculatorIcon.style.opacity = '1';
     }
 });
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteChar() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+        setTimeout(clearDisplay, 1000);
+    }
+}
